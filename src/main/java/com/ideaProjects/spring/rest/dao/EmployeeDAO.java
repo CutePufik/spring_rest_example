aa46552dd552f7d09f8d7af26780eac6859c89c1
Александр Черевко <acherevko@test.ru>
2026-01-21 20:16:03 +0300

    Initial commit

---
+++ src/main/java/com/ideaProjects/spring/rest/dao/EmployeeDAO.java
@@ -0,0 +1,18 @@
+package com.ideaProjects.spring.rest.dao;
+
+
+
+import com.ideaProjects.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
+}
